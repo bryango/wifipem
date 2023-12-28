018ade8527b7f782d08d22fb07d4f7622e4806a4
Bryan Lai <bryanlais@gmail.com>
2023-12-28 23:24:01 +0800

    simplified code further

--- wifipem.py
+++ wifipem.py
@@ -1,59 +1,64 @@
 #!/usr/bin/python3
 
 import argparse
+import pyshark
+import binascii
 
 ## Settings
 __version__ = '2.0'
-radius_certificate_extract_location = 'radius.der'
-default_timeout = 15
+certificate_prefix = 'cert'
 
-parser = argparse.ArgumentParser(description='Automated tool for extract the public key presented by WPA2-Enterprise wireless networks')
+parser = argparse.ArgumentParser(description='Automated tool to extract the public key presented by WPA2-Enterprise wireless networks')
 
 parser.add_argument('--version', action='version', version=__version__)
-parser.add_argument('-o', '--output', dest='output_file', default=radius_certificate_extract_location, help='Specify the output file (Default: {})'.format(radius_certificate_extract_location))
-
-sourceOptions = parser.add_argument_group(description='Specify target source for extraction')
-sourceOptions.add_argument('-f', '--filename', dest='filename', help='extract .pem from a pcap')
+parser.add_argument('-o', '--output', dest='output_prefix', default=certificate_prefix, help='filename prefix of the .der output (default: {})'.format(certificate_prefix))
+parser.add_argument('-i', '--input', dest='input_filename', help='.pcap file to be extracted')
 
 args, leftover = parser.parse_known_args()
 options = args.__dict__
 
-def pcapExtraction(filename: str, output_file):
-    import pyshark
-    import binascii
-    packets = pyshark.FileCapture(filename)
-    count = 1
+def pcapExtraction(input_filename: str, output_prefix: str):
+    packets = pyshark.FileCapture(input_filename)
     certificates = []
+    count = 1
     for pkt in packets:
-        if(('EAP' in pkt)):
-            if((int(pkt['EAP'].code) == 1) and (hasattr(pkt['EAP'], 'tls_handshake_certificate'))):
-                print('[-]  certificate frame found!')
-                for cert in pkt['EAP'].tls_handshake_certificate.all_fields:
-                    certificates.append(cert)
-                    hex_array = [
-                        cert.raw_value[i:i+2]
-                        for i in range(0, len(cert.raw_value), 2)
-                    ]
-                    file = '{}-{}-{}.pem'.format(filename.split('.')[0], pkt['WLAN'].ta, count)
-                    print('[-]  extracting certificate to file: {}'.format(file))
-                    with open(file, 'wb') as f:
-                        for ha in hex_array:
-                            f.write(
-                                binascii.unhexlify(ha)
-                            )
-                        f.close()
-                    print('[-]  open file with the following command:\r\n[-]    openssl x509 -inform der -in {} -text'.format(file))
-                    count += 1
+        if 'EAP' in pkt \
+        and int(pkt['EAP'].code) == 1 \
+        and hasattr(pkt['EAP'], 'tls_handshake_certificate'):
+            print('[-]  certificate frame found!')
+        else:
+            continue
+
+        for cert in pkt['EAP'].tls_handshake_certificate.all_fields:
+            certificates.append(cert)
+            hex_array = [
+                cert.raw_value[i:i+2]
+                for i in range(0, len(cert.raw_value), 2)
+            ]
+            file = '{}-{}-{}-{}.der'.format(
+                output_prefix,
+                ".".join(
+                    input_filename.split('/')[-1].split('.')[:-1]
+                ),
+                pkt['WLAN'].ta,
+                count
+            )
+            print('[-]  extracting certificate to file: {}'.format(file))
+            with open(file, 'wb') as f:
+                for ha in hex_array:
+                    f.write(binascii.unhexlify(ha))
+            print('[-]  open file with the following command:\r\n[-]    openssl x509 -inform der -in {} -text'.format(file))
+            count += 1
     return 0
 
 if __name__ == '__main__':
-    if(options['filename'] is None):
+    if(options['input_filename'] is None):
         print('[!] Select one file for extraction')
         exit(0)
 
-    print('[+] Searching for RADIUS public certificate in file: {}'.format(options['filename']))
+    print('[+] Searching for RADIUS public certificate in file: {}'.format(options['input_filename']))
     pcapExtraction(
-        filename=options['filename'],
-        output_file=options['output_file']
+        input_filename=options['input_filename'],
+        output_prefix=options['output_prefix']
     )
     exit(0)
